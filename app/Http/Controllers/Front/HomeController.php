83a64eca233e070d196e8bc14cb6a65d79ed613e
local <local@example.com>
2025-09-26 15:40:12 +0100

    Structure: isolate front controllers under Front/ and backoffice under Backoffice/, update routes

---
+++ app/Http/Controllers/Front/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Front;
+
+use App\Http\Controllers\Controller;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        return view('front.home');
+    }
+}
+
+
