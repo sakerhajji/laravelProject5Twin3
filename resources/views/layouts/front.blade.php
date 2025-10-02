69ddcb3dfad0f2c801cf9e04f3e9cf971f3a4c3f
TuniCoder <sofiennemrabet16@gmail.com>
2025-10-02 21:21:21 +0100

    fix navbar front

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -67,6 +67,7 @@
                     <li class="nav-item"><a class="nav-link" href="{{ url('/') }}">Accueil</a></li>
                     @auth
                         <!-- Health Dropdown Menu -->
+                        <li class="nav-item"><a class="nav-link" href="{{ route('front.smart-dashboard.index') }}">Dashboard IA</a></li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="healthDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 <i class="fas fa-heartbeat text-danger me-1"></i>Santé & Bien-être
